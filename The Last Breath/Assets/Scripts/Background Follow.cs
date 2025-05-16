269b00719ff675c8d370dd83fb9d162a277b7734
MarioSantosHeredia <mariosantosheredia@uma.es>
2025-05-16 21:19:08 +0200

    Escena Cementerio

---
+++ The Last Breath/Assets/Scripts/Background Follow.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class BackgroundFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public float fixedY = 0f; // Y fijo para la camara
+    public float minX, maxX; // limites horizontales
+    public Vector3 offset;
+
+    public bool lockCamera = false;
+    public Vector3 lockedPosition;
+
+    void LateUpdate()
+    {
+        if (lockCamera)
+        {
+            Vector3 targetPosition = new Vector3(lockedPosition.x, fixedY + offset.y, transform.position.z);
+            Vector3 smoothed = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+            transform.position = smoothed;
+            return;
+        }
+
+        if (target == null) return;
+
+        float clampedX = Mathf.Clamp(target.position.x + offset.x, minX, maxX);
+
+        Vector3 desiredPosition = new Vector3(clampedX, fixedY + offset.y, transform.position.z);
+        
+        Vector3 smooth = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smooth;
+    }
+}
