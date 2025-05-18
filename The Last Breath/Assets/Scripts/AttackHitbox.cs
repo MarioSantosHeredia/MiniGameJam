75c88089849e66d06d8461b4c975c55f6c8c765d
MarioSantosHeredia <mariosantosheredia@uma.es>
2025-05-18 21:36:40 +0200

    Nosequeponerxd

--- The Last Breath/Assets/Scripts/AttackHitbox.cs
+++ The Last Breath/Assets/Scripts/AttackHitbox.cs
@@ -4,17 +4,26 @@ public class AttackHitbox : MonoBehaviour
 {
     public float damage = 10f;
 
+    public EfectosSonido soundManager;
+
+    private void Start()
+    {
+        soundManager = FindFirstObjectByType<EfectosSonido>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent<EnemyHealth>(out var enemy))
         {
             enemy.TakeDamage(damage);
+            soundManager.SeleccionarSonido(4, 1f);
         }
 
         // Si es el boss, danarlo tambien
         if (other.TryGetComponent<HealthBoss>(out var boss))
         {
             boss.TakeDamage(damage);
+            soundManager.SeleccionarSonido(4, 1f);
         }
     }
 }
